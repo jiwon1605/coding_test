4613e164dc58e71d779b760aaed6afb5cfe5b694
jiwon1605 <122809097+jiwon1605@users.noreply.github.com>
2024-09-03 18:40:58 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int main(void) {
+    string s;
+    int point;
+
+    cin >> s;
+    cin >> point;
+    int P = point - 1;
+    cout << s[P];
+}
