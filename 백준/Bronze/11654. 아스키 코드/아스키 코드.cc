77c1825d372ed6e934c6a606ac8a0d6430743712
jiwon1605 <122809097+jiwon1605@users.noreply.github.com>
2024-09-03 22:45:34 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int main(void) {
+	char ch;
+	cin >> ch;
+
+	cout << (int(ch));
+}
