83e9917d5d7f3d7edf5532a1647654fb6ef4661b
jiwon1605 <122809097+jiwon1605@users.noreply.github.com>
2024-09-06 23:33:09 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int main(void) {
+	string input;
+	int index = 0;
+
+	do {
+		getline(cin, input);
+		cout << input<<endl;
+		if (input == "")
+			break;
+	} while (exit);
+
+
+}
