7585550c75a35c5c71acfaf029307a33fc51dd64
jiwon1605 <122809097+jiwon1605@users.noreply.github.com>
2025-06-17 19:21:55 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    
+    cin>>a;
+    cin>>b;
+    
+    int result;
+    result = (a+b)*(a-b);
+    
+    cout<<result;
+    
+    return 0;
+}
